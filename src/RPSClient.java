8c5b555b3a3231d7c3baaf8f57d3be5673b3e32d
johnst101 <107722987+johnst101@users.noreply.github.com>
2024-04-10 22:31:45 -0500

    Setting up the class structure and a couple of methods.

--- src/RPSClient.java
+++ src/RPSClient.java
@@ -1,2 +1,34 @@
-public class RPSClient {
+/**
+ * TODO
+ *
+ * @author Tyler Johnson (tjohson)
+ * @version 1.0 Apr 10, 2024
+ */
+
+// Packages //
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RPSClient extends Application {
+    /**
+     * TODO
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     * the application scene can be set. The primary stage will be embedded in
+     * the browser if the application was launched as an applet.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages and will not be embedded in the browser.
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    /**
+     * TODO
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/RPSServer.java
+++ src/RPSServer.java
@@ -1,5 +1,44 @@
-public class RPSServer {
+/**
+ * TODO
+ *
+ * @author Tyler Johnson (tjohson)
+ * @version 1.0 Apr 10, 2024
+ */
+
+// Packages //
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RPSServer extends Application {
+    /**
+     * TODO
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     * the application scene can be set. The primary stage will be embedded in
+     * the browser if the application was launched as an applet.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages and will not be embedded in the browser.
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    /**
+     * TODO
+     */
+    class GameSession implements Runnable {
+        /**
+         * TODO
+         */
+        @Override
+        public void run() {
+
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
